2e9168b8f9e322e920d939cfa2a679b575240a90
Joseph <josephm@maine.rr.com>
2014-12-02 15:31:11 -0500

    Trying out ORB/FREAK for object detection

--- src/object_recognizer_node.cpp
+++ src/object_recognizer_node.cpp
@@ -5,6 +5,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
 float h_range[] = {0,256};
 float s_range[] = {0,256};
@@ -22,11 +24,23 @@ private:
     cv::Mat obj_hist; // Object histogram
     cv::Mat back_hist; // background histogram
 
+    cv::Ptr<cv::FeatureDetector> detector;
+    cv::Ptr<cv::DescriptorExtractor> extractor;
+    cv::Mat obj_descriptors;
+    cv::Mat obj_img;
+    std::vector<cv::KeyPoint> obj_keypoints;
+
     image_transport::ImageTransport it;
     image_transport::Subscriber image_sub;
 public:
-    ColorStage(ros::NodeHandle nh, cv::Mat hist, cv::Mat back_hist) : it(nh), obj_hist(hist), back_hist(back_hist) {
+    ColorStage(ros::NodeHandle nh, std::string calib_image, cv::Mat hist, cv::Mat back_hist) : it(nh), obj_hist(hist), back_hist(back_hist) {
         image_sub = it.subscribe("/softkinetic_camera/color", 1, &ColorStage::process_frame, this);
+
+        detector = cv::FeatureDetector::create("ORB");
+        extractor = cv::DescriptorExtractor::create("FREAK");
+        obj_img = cv::imread(calib_image, CV_LOAD_IMAGE_GRAYSCALE);
+        detector->detect(obj_img, obj_keypoints);
+        extractor->compute(obj_img, obj_keypoints, obj_descriptors);
     }
 
     void process_frame(const sensor_msgs::ImageConstPtr& msg) {
@@ -39,9 +53,91 @@ public:
             return;
         }
 
-        cv::imshow("Camera", img_ptr->image);
+        cv::Mat img;
+        cv::Mat color;
+        cv::cvtColor(img_ptr->image, img, CV_BGR2GRAY);
+        color = img_ptr->image;
+        std::vector<cv::KeyPoint> img_keypoints;
+        cv::Mat img_descriptors;
+
+        detector->detect(img, img_keypoints);
+        extractor->compute(img, img_keypoints, img_descriptors);
+
+        //cv::FlannBasedMatcher matcher;
+        //cv::BruteForceMatcher<cv::Hamming> matcher;
+        cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
+        std::vector<cv::DMatch> matches;
+        matcher->match(obj_descriptors, img_descriptors, matches);
+
+        double max_dist = 0; double min_dist = 100;
+
+        //-- Quick calculation of max and min distances between keypoints
+        for( int i = 0; i < obj_descriptors.rows; i++ )
+        { 
+            double dist = matches[i].distance;
+            if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+        }
+
+        //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+        std::vector<cv::DMatch> good_matches;
+
+        for( int i = 0; i < obj_descriptors.rows; i++ )
+        { 
+            if( matches[i].distance < 3*min_dist ) { 
+                good_matches.push_back( matches[i]); }
+        }
+
+        cv::Mat img_matches;
+        cv::drawMatches( obj_img, obj_keypoints, img, img_keypoints,
+                     good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+                     std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+        //-- Localize the object
+        std::vector<cv::Point2f> obj;
+        std::vector<cv::Point2f> scene;
+
+        for( int i = 0; i < good_matches.size(); i++ )
+        {
+            //-- Get the keypoints from the good matches
+            obj.push_back( obj_keypoints[ good_matches[i].queryIdx ].pt );
+            scene.push_back( img_keypoints[ good_matches[i].trainIdx ].pt );
+        }
+
+        cv::Mat H = cv::findHomography( obj, scene, CV_RANSAC );
+
+        if(!H.empty()) {
+
+            //-- Get the corners from the image_1 ( the object to be "detected" )
+            std::vector<cv::Point2f> obj_corners(4);
+            obj_corners[0] = cvPoint(0,0); 
+            obj_corners[1] = cvPoint( obj_img.cols, 0 );
+            obj_corners[2] = cvPoint( obj_img.cols, obj_img.rows ); 
+            obj_corners[3] = cvPoint( 0, obj_img.rows );
+
+            std::vector<cv::Point2f> scene_corners(4);
+
+            perspectiveTransform( obj_corners, scene_corners, H);
+
+            //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+            cv::line( color, scene_corners[0], scene_corners[1], cv::Scalar(0, 255, 0), 4 );
+            cv::line( color, scene_corners[1], scene_corners[2], cv::Scalar( 0, 255, 0), 4 );
+            cv::line( color, scene_corners[2], scene_corners[3], cv::Scalar( 0, 255, 0), 4 );
+            cv::line( color, scene_corners[3], scene_corners[0], cv::Scalar( 0, 255, 0), 4 );
+            /*
+            cv::line( color, scene_corners[0] + cv::Point2f( obj_img.cols, 0), scene_corners[1] + cv::Point2f( obj_img.cols, 0), cv::Scalar(0, 255, 0), 4 );
+            cv::line( color, scene_corners[1] + cv::Point2f( obj_img.cols, 0), scene_corners[2] + cv::Point2f( obj_img.cols, 0), cv::Scalar( 0, 255, 0), 4 );
+            cv::line( color, scene_corners[2] + cv::Point2f( obj_img.cols, 0), scene_corners[3] + cv::Point2f( obj_img.cols, 0), cv::Scalar( 0, 255, 0), 4 );
+            cv::line( color, scene_corners[3] + cv::Point2f( obj_img.cols, 0), scene_corners[0] + cv::Point2f( obj_img.cols, 0), cv::Scalar( 0, 255, 0), 4 );
+            */
+        }
+        
+        //-- Show detected matches
+
+        cv::imshow("OUT", color);
         cv::waitKey(3);
 
+        /*
         // Save image size
         cv::Size img_size = img_ptr->image.size();
 
@@ -63,10 +159,11 @@ public:
         // 
         binary = obj_backhist > back_weight*back_backhist;
 
-        cv::imshow("Output", binary);
-        cv::imshow("HIST", obj_backhist);
-        cv::imshow("BAKC HIST", back_backhist);
+        //cv::imshow("Output", binary);
+        //cv::imshow("HIST", obj_backhist);
+        //cv::imshow("BAKC HIST", back_backhist);
         cv::waitKey(3);
+        */
     }
 };
 
@@ -80,8 +177,9 @@ int main(int argc, char** argv) {
     }
 
     cv::Mat hist, backHist;
-    cv::FileStorage fs(argv[1], cv::FileStorage::READ);
+    //cv::FileStorage fs(argv[1], cv::FileStorage::READ);
 
+    /*
     if(!fs.isOpened()) {
         std::cout << "Failed to open histogram file\n";
         return -1;
@@ -89,8 +187,9 @@ int main(int argc, char** argv) {
 
     fs["hist"] >> hist;
     fs["back_hist"] >> backHist;
+    */
 
 
-    ColorStage cs(nh, hist, backHist);
+    ColorStage cs(nh, argv[1], hist, backHist);
     ros::spin();
 }
